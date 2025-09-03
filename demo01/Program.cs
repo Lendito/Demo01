b9bc01ee145756405cd6841314ac625b09fab22d
Angelo <7895123aszx@mail.com>
2025-09-03 09:00:05 -0500

    commit2

--- demo01/Program.cs
+++ demo01/Program.cs
@@ -10,6 +10,16 @@ namespace demo01
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Hola, ¿Cual es tu nombre?: ");
+            string nombre = Console.ReadLine();
+
+            Console.Write("Cual es tu edad?: ");
+            int edad = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("¿Cual es tu estatura?: ");
+            decimal estatura = Convert.ToDecimal(Console.ReadLine());
+
+            Console.WriteLine($"mi nombre es {nombre}, mi edad es {edad} y mi estatura es {estatura:N2}");
         }
     }
 }
